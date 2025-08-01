9b8f0a22f45c801d6a7396857efcdfe71a93fd23
themanjeetkr <themanjeetkr@gmail.com>
2025-08-01 11:32:06 +0530

    add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from '/favicon.svg'; // ✅ Fixed
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-  <h1 className="text-3xl font-bold underline">
+    <div className="text-3xl font-bold underline">
       Hello world!
-    </h1>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
